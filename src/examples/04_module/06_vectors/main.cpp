99d77f066b3a7b4e97edcf7996291916405793e8
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-09-29 19:09:14 -0500

    vector examples

--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,30 @@
 #include "vec.h"
+#include<vector>
+#include<iostream>
+
+using std::cout;
+using std::vector;
 
 int main() 
 {
+	vector<int> nums;
+
+	cout<<"Size of nums: "<<nums.size()<<"\n";
+	cout<<"Capacity of nums: "<<nums.capacity()<<"\n";
+	cout<<"Max size of nums: "<<nums.max_size()<<"\n";
+	nums.push_back(15);
+	cout<<"Capacity of nums: "<<nums.capacity()<<"\n\n";
+	
+	vector<int> nums2(10, 5);
+
+	for(auto n: nums)
+	{
+		cout<<n<<"\n";
+	}
+
+	cout<<nums[0]<<"\n";
+	nums[0] = 20;
+	cout<<nums[0]<<"\n";
 
 	return 0;
 }
