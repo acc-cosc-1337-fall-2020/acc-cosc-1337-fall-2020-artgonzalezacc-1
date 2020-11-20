14428f7f3eac0dabb36cc10ba95b84709b4f5834
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-11-20 10:04:34 -0600

    pointer tests

--- test/examples_test/09_module_test/09_module_tests.cpp
+++ test/examples_test/09_module_test/09_module_tests.cpp
@@ -1,8 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "types.h"
+#include "ref_pointers.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Reference and pointer function parameters")
+{
+	int n1 = 5, n2 = 10;
+	
+	ref_param(n1, &n2);
+
+	REQUIRE(n1 == 100);
+	REQUIRE(n2 == 200);
+}
+
+TEST_CASE("Reference and pointer function parameters use a pointer")
+{
+	int n1 = 5, n2 = 10;
+	int* ptr = &n2;
+	
+	ref_param(n1, ptr);
+
+	REQUIRE(n1 == 100);
+	REQUIRE(n2 == 200);
 }
 
