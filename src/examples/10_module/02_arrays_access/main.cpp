ea3f46d3f3226d25dfcbc1b7f6437f5a5b7455b1
artgonzalezacc <arturo.gonzalez@austincc.edu>
2020-12-09 00:21:11 -0600

    analysi: linear search, binary, bubble sort

--- src/examples/10_module/02_arrays_access/main.cpp
+++ src/examples/10_module/02_arrays_access/main.cpp
@@ -1,3 +1,29 @@
-int main()
-{
-}
+#include "arrays_access.h"
+#include <iostream>
+#include <chrono>
+
+int main ()
+{	
+
+  const int SIZE = 1000000;
+  int* list = new int[SIZE];
+  populate_array(list, SIZE, false);
+
+  std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+  std::cout<<"function executing...: \n";
+  //100000,250000,500000,1000000
+  //linear_search(list, SIZE, -5);
+  //10000,100000,200000
+  //binary_search(list, SIZE, -5);
+  //10000,100000,200000
+  bubble_sort(list, SIZE);
+  std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+  std::cout<<"function done: \n";
+
+  std::cout<<"Milliseconds: "<<std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
+
+  delete list;
+
+  return 0;
+} 	
